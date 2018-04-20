caef1add95af6af3ab49d243577d7f4a064e5cc1
udon <naoki_udon@outlook.jp>
2018-04-21 01:04:58 +0900

    create base class

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MoveController : MonoBehaviour {
+
+    [SerializeField]
+    private Rigidbody _rigidbody;
+
+    public void SetMoveDirection(){
+
+    }
+
+}
